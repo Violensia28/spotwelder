8db2528422e225fd37dac2fe66fa21920a1d0823
Violensia28 <Violensia28@users.noreply.github.com>
2025-10-02 19:35:26 +0000

    deploy: 67d32b502681115e4d0b37f53da6b19aac469067

---
+++ FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/runner/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
